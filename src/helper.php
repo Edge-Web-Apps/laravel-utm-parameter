929f65e8ce30ba024dc147193263b0bbaf5e8bcc
Bri MacBain <35091265+brimacbain@users.noreply.github.com>
2026-05-05 23:32:33 -0700

    Revert "updates from main"
    This reverts commit 69ce202774beba226a4a16dfd237d1367ae7820b.

--- src/helper.php
+++ src/helper.php
@@ -2,44 +2,45 @@
 
 use Suarez\UtmParameter\Facades\UtmParameter;
 
-if (! function_exists('get_all_utm')) {
-    function get_all_utm(): array
+if (!function_exists('get_all_utm')) {
+    function get_all_utm()
     {
         return UtmParameter::all();
     }
 }
 
-if (! function_exists('get_utm')) {
-    function get_utm($key): ?string
+if (!function_exists('get_utm')) {
+    function get_utm($key)
     {
         return UtmParameter::get($key);
     }
 }
 
-if (! function_exists('has_utm')) {
-    function has_utm($key, $value = null): bool
+if (!function_exists('has_utm')) {
+    function has_utm($key, $value = null)
     {
         return UtmParameter::has($key, $value);
     }
 }
 
-if (! function_exists('has_not_utm')) {
-    function has_not_utm($key, $value = null): bool
+if (!function_exists('has_not_utm')) {
+    function has_not_utm($key, $value = null)
     {
-        return ! UtmParameter::has($key, $value);
+        return !UtmParameter::has($key, $value);
     }
 }
 
-if (! function_exists('contains_utm')) {
-    function contains_utm($key, $value): bool
+
+if (!function_exists('contains_utm')) {
+    function contains_utm($key, $value)
     {
         return UtmParameter::contains($key, $value);
     }
 }
 
-if (! function_exists('contains_not_utm')) {
-    function contains_not_utm($key, $value): bool
+if (!function_exists('contains_not_utm')) {
+    function contains_not_utm($key, $value)
     {
-        return ! UtmParameter::contains($key, $value);
+        return !UtmParameter::contains($key, $value);
     }
 }
